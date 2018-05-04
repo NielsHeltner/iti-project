18800f82c6c992068f5f4d77c33872bdb63d8eaa
Unknown <lascaritheman@gmail.com>
2018-05-04 21:50:33 +0200

    Newsletter page implemented
    lol Nic er grim

---
+++ iti-project/Data/Email.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace iti_project.Data
+{
+    [Table("newsletter")]
+    public class Email
+    {
+        [Key]
+        public string email { get; set; }
+
+        public Email(string email)
+        {
+            this.email = email;
+        }
+    }
+}
---
+++ iti-project/Pages/Newsletter.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using iti_project.Data;
+
+namespace iti_project.Pages
+{
+    public class NewsletterModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost()
+        {
+            var emailadress = Request.Form["email"];
+
+            using (var db = new DatabaseContext())
+            {
+                try
+                {
+                    db.Emails.Add(new Email(emailadress));
+                    db.SaveChanges();
+                    ViewData["confirmation"] = $"{emailadress} is registered!";
+                }
+                catch (Microsoft.EntityFrameworkCore.DbUpdateException e)
+                {
+                    ViewData["confirmation"] = $"{emailadress} was NOT registered - This email is already registered!";
+                }
+                catch (Exception e)
+                {
+                    ViewData["confirmation"] = $"{emailadress} was NOT registered - Unknown error!";
+                }
+            }
+        }
+    }
+}
