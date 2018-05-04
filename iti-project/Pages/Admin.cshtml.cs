478a9d9fc274b708b113e2e31d9971288d67dc7a
Niels Heltner <niels.heltner@hotmail.com>
2018-05-05 00:47:41 +0200

    added forms to edit about-me page

--- iti-project/Pages/Admin.cshtml.cs
+++ iti-project/Pages/Admin.cshtml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using iti_project.Data;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -14,9 +15,26 @@ namespace iti_project.Pages
 
         [BindProperty]
         public IFormFile Upload { get; set; }
+        public List<User> Users { get; set; }
+
+        public User GetUserByName(string name)
+        {
+            foreach(User user in Users)
+            {
+                if(user.Name.Equals(name))
+                {
+                    return user;
+                }
+            }
+            return null;
+        }
 
         public void OnGet()
         {
+            using (var db = new DatabaseContext())
+            {
+                Users = db.Users.ToList();
+            }
         }
 
         public async Task OnPostAsync()
