4d31af27acb3a46c967f5df40cc8473ffe617b04
Niclas Mølby <niclas@molby.net>
2018-05-04 23:04:46 +0200

    Upload file

---
+++ iti-project/Pages/Admin.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace iti_project.Pages
+{
+    public class AdminModel : PageModel
+    {
+
+        [BindProperty]
+        public IFormFile Upload { get; set; }
+
+        public void OnGet()
+        {
+        }
+
+        public async Task OnPostAsync()
+        {
+            var file = Path.Combine("wwwroot/images/gallery/", Upload.FileName);
+            using (var fileStream = new FileStream(file, FileMode.Create))
+            {
+                await Upload.CopyToAsync(fileStream);
+            }
+        }
+    }
+}
